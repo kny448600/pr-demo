513c1e3afde52166d1a32a63f014acb53b659445
kny448600 <kny448600@gmail.com>
2024-09-20 13:49:08 +0900

    9_20

--- public/memory/app.js
+++ public/memory/app.js
@@ -1,123 +1,113 @@
-const stageSize = 15;
-
-// 스테이지 생성
-for (let i = 0; i < stageSize*stageSize; i++) {
-    const box = document.createElement("div");
-    document.querySelector(".grid").appendChild(box);
-}
-
-const grid = document.querySelector(".grid");
-const stage = document.querySelectorAll(".grid div");
-const statBtn = document.querySelector("#startBtn");
-const stopBtn = document.querySelector("#stopBtn");
-const display = document.querySelector("#display");
-
-let playerLoc = stageSize*(stageSize-2)+7 // 202;
-let invadersLoc = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+const cards = [
+    {
+        name : 'Choonsik1',
+        img : 'img/앞모습1.gif'
+    },
+    {
+        name : 'Choonsik1',
+        img : 'img/앞모습1.gif'
+    },
+    {
+        name : 'Choonsik2',
+        img : 'img/앞모습2.gif'
+    },
+    {
+        name : 'Choonsik2',
+        img : 'img/앞모습2.gif'
+    },
+    {
+        name : 'Choonsik3',
+        img : 'img/앞모습3.gif'
+    },
+    {
+        name : 'Choonsik3',
+        img : 'img/앞모습3.gif'
+    },
+    {
+        name : 'Choonsik4',
+        img : 'img/앞모습4.gif'
+    },
+    {
+        name : 'Choonsik4',
+        img : 'img/앞모습4.gif'
+    },
+    {
+        name : 'Choonsik5',
+        img : 'img/앞모습5.gif'
+    },
+    {
+        name : 'Choonsik5',
+        img : 'img/앞모습5.gif'
+    },
+    {
+        name : 'Choonsik6',
+        img : 'img/앞모습6.gif'
+    },
+    {
+        name : 'Choonsik6',
+        img : 'img/앞모습6.gif'
+    }
 ]
-let gameInterval;
 
-function makePlayer() {
-    stage[playerLoc].classList.add("player");
-}
+document.addEventListener('DOMContentLoaded', ()=> {
+    cards.sort(() => 0.5 - Math.random())
 
-function movePlayer(e) {
-    stage[playerLoc].classList.remove("player");
-    switch (e.keyCode) {
-        case 37: //왼쪽
-            if (playerLoc % stageSize !== 0) {playerLoc--;}
-            break;
-        case 39: //오른쪽
-            if (playerLoc % stageSize < stageSize-1) {playerLoc++;}
-            break;
-    }
-    stage[playerLoc].classList.add("player");
-}
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
 
-function makeInvader() {
-    invadersLoc.forEach(function(invader) {
-        stage[invader].classList.add("invader");
-    })
-}
-
-
-function moveInvader() {
-    invadersLoc.forEach(function(invader) {
-        stage[invader].classList.remove("invader");
-    })
-    for(let i=0; i < invadersLoc.length; i++) {
-        invadersLoc[i]++;
-        stage[invadersLoc[i]].classList.add("invader");
-    }
-}
-
-function gameStart() {
-    stage[playerLoc].classList.remove("player");
-    invadersLoc.forEach(function(invader) {
-        stage[invader].classList.remove("invader");
-    });
-    display.innerText = ""
-    playerLoc = stageSize*(stageSize - 2)+7 //202
-    invadersLoc = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-    ]
-    makePlayer();
-    makeInvader();
-    displayStatus();
-    gameInterval = setInterval(moveInvader, 1000);
-    document.addEventListener("keyup",movePlayer);
-    gameRun();
-}
-
-function gameStop() {
-    clearInterval(gameInterval);
-    document.removeEventListener("keyup", movePlayer);
-}
-
-function gameRun() {
-    moveInvader();
-}
-
-function displayStatus() {
-    display.innerText = invadersLoc.length + "/" + invadersLoc.length
-}
-
-startBtn.addEventListener("click", gameStart);
-stopBtn.addEventListener("click",gameStop);
-
-let bulletId;
-let bulletLoc = playerLoc;
-function moveBullet() {
-    stage[bulletLoc].classList.remove("bullet");
-    bulletLoc -= stageSize;
-    stage[bulletLoc].classList.add("bullet");
-}
-
-function shoot(e) {
-    let id;
-    let bulletLoc = playerLoc;
-    function moveBullet() {
-        bulletLoc -= stageSize;
-        if(bulletLoc < 0) {
-            clearInterval(id);
-            return;
+    function createBoard() {
+        for (let i = 0; i < cards.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'img/뒷모습.jpg')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click',flipCard)
+            grid.appendChild(card)
         }
-        stage[bulletLoc].classList.remove("bullet");
-        if (stage[bulletLoc].classList.contains("invader")) {
-            stage[bulletLoc].classList.remove("invader")
-            stage[bulletLoc].classList.remove("bullet")
-            stage[bulletLoc].classList.add("boom")
-        }
-        stage[bulletLoc].classList.add("bullet");
-    }
-    if (e.key === "ArrowUp") {
-        id = setInterval(moveBullet, 300)
     }
 
-}
-document.addEventListener("keydown", shoot);
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cards[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cards[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if(optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'img/뒷모습.jpg')
+            cards[optionTwoId].setAttribute('src', 'img/뒷모습.jpg')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            cards[optionOneId].setAttribute('src', 'img/하얀 카드.jpg')
+            cards[optionTwoId].setAttribute('src', 'img/하얀 카드.jpg')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+            alert('You found a match')
+        }
+        else {
+            cards[optionOneId].setAttribute('src', 'img/뒷모습.jpg')
+            cards[optionTwoId].setAttribute('src', 'img/뒷모습.jpg')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cards.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+
+    }
+    createBoard();
+})
